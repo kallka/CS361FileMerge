03b5510cad9b6136dea74044dc17435b6a0511d1
kallka <86282863+kallka@users.noreply.github.com>
2023-03-25 17:44:32 -0700

    Add files via upload

--- MessagesError.py
+++ MessagesError.py
@@ -1,8 +1,22 @@
+# Name: Karina Kallas
+# Class: CS361
+# Last Edited: March 9, 2023
+# File Name: File Merge Home UI
+# Description: Error messages that are used frequently and can be called by function.
+
+
+# --------------------------- #
+# --------- FREQUENT  ------- #
+# --------- ERROR  ---------- #
+# --------- MESSAGES  ------- #
+# --------------------------- #
 def general_error():
     return "Oops! There was an error."
 
+
 def entry_error():
     return "Oops! You forgot to fill in some fields. Please try again."
+
 
 def try_again():
     return "Let's try that again ..."
--- MessagesSuccess.py
+++ MessagesSuccess.py
@@ -1,5 +1,18 @@
+# Name: Karina Kallas
+# Class: CS361
+# Last Edited: March 9, 2023
+# File Name: File Merge Home UI
+# Description: Error messages that are used frequently and can be called by function.
+
+
+# --------------------------- #
+# --------- FREQUENT  ------- #
+# --------- SUCCESS  -------- #
+# --------- MESSAGES  ------- #
+# --------------------------- #
 def general_success():
     return "Success!"
+
 
 def input_entered():
     return "Your input has been successfully entered!"
